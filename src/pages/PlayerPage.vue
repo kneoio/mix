03530c19d8c46f2815d7419ae56ad71e3578f0bd
kaira <justaidajam@gmail.com>
2025-10-19 12:47:22 +0100

    #

--- src/pages/PlayerPage.vue
+++ src/pages/PlayerPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-dialog v-model="dialogModel" maximized transition-show="slide-up" transition-hide="slide-down">
     <q-card>
-      <q-bar class="bg-accent text-white">
+      <q-bar class="mixpla-header-gradient text-white">
         <div class="text-h6 mixpla-title">Mixpla</div>
         <q-space />
         <q-btn flat dense round icon="expand_less" v-close-popup class="lt-sm" />
@@ -20,7 +20,7 @@
           <q-btn
             round
             size="xl"
-            color="accent"
+            color="grey-5"
             :icon="isPlaying ? 'pause' : 'play_arrow'"
             @click="togglePlay"
           />
